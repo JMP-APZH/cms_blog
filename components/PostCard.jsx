00bce9d8b87b9a62cce37f4c727cc7f0878dbf67
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-02-19 16:12:18 +0100

    First PostCard

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import moment from 'moment'
+import Link from 'next/link';
 
 const PostCard = ({ post }) => {
-  console.log(post);
+  console.log(post.title);
   return (
     <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
         <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
@@ -12,6 +13,14 @@ const PostCard = ({ post }) => {
             className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
           />
         </div>
+        <h1 
+          className='transition duration-700 text-center mb-8 
+          hover:text-pink-600 text-3xl font-semibold cursor-pointer'>
+            <Link href={`/post/${post.slug}`}>
+              {post.title}
+            </Link>
+        </h1>
+        
     </div>
   )
 }
