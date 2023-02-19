e3e434925f6a320bfbaef9cd3ea9a8fa40ea7fba
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-02-19 17:56:45 +0100

    recent and similar posts queries

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import Link from 'next/link';
 
-const PostWidget = () => {
+import { getRecentPosts, getSimilarPosts } from '../services';
+
+const PostWidget = ( {categories, slug} ) => {
+  const [relatedPosts, setrelatedPosts] = useState([])
+
+  useEffect(() => {
+    if(slug) {
+      getSimilarPosts(categories, slug)
+        .then((results) => setrelatedPosts(results))
+    } else {
+      getRecentPosts()
+        .then((results) => setrelatedPosts(results))
+    }
+  
+    // return () => {
+    //   second
+    // }
+  }, [slug])
+  
+  console.log(relatedPosts)
+
   return (
     <div>PostWidget</div>
   )
