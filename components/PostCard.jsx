acaaa1d36fbd705705c01288f9dc19138f1b581f
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-02-19 12:47:56 +0100

    created main components: PostCard, PostWidget, Categories

---
+++ components/PostCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const 
+    PostCard = ({ post }) => {
+  return (
+    <div>
+        PostCard
+        {post.title}
+        {post.excerpt}
+    </div>
+  )
+}
+
+export default 
+    PostCard
