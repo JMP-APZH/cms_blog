b3098d3d17ca85c5ac1e55aa3bfcbc8b313a4366
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-02-21 13:48:32 +0100

    PostDetails - Author info displayed

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-    <h1>Author</h1>
+    
+    <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20'>
+      <div className='flex justify-center absolute right-0 left-0 -top-16'>
+        <Image 
+          alt={author.name}
+          unoptimized
+          height={100}
+          width={100}
+          className='align-middle rounded-full'
+          src={author.photo.url}
+        />
+      </div>
+      <h3 className='text-white my-4 text-xl font-bold'>
+        {author.name}
+      </h3>
+      <p className='text-white'> {author.bio} </p>
+    </div>
   )
 }
 
