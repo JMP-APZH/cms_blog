a9f613a82f031d98a1f1138151af8f5aff02e72a
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-02-22 11:09:49 +0100

    comment retrieval preparation in frontend + services query getcomments

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import moment from 'moment'
+import parse from 'html-react-parser'
 
-const Comments = () => {
+const Comments = ({ slug }) => {
+
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    // getComments(slug)
+  }, [])
+  
+
   return (
     <h1>Comments</h1>
   )
