f408ad54251eff2c5fbfce070b78cedbfa8e78e7
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-02-22 11:00:21 +0100

    comment creation in frontend completed

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
+import { submitComment } from '../services';
 
 const CommentsForm = ({ slug }) => {
 
@@ -10,6 +11,12 @@ const CommentsForm = ({ slug }) => {
   const nameEl = useRef();
   const emailEl = useRef();
   const storeDataEl = useRef();
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem('name')
+    emailEl.current.value = window.localStorage.getItem('email')
+  }, [])
+  
 
   const handleCommentSubmission = () => {
     setError(false)
@@ -29,19 +36,28 @@ const CommentsForm = ({ slug }) => {
     }
 
     if(storeData) {
-      localStorage.setItem('name', name)
-      localStorage.setItem('email', email)
+      window.localStorage.setItem('name', name)
+      window.localStorage.setItem('email', email)
     } else {
-      localStorage.removeItem('name', name)
-      localStorage.removeItem('email', email)
+      window.localStorage.removeItem('name', name)
+      window.localStorage.removeItem('email', email)
     }
+
+    submitComment(commentObj)
+      .then((res) => {
+        setShowSuccessMessage(true)
+
+        setTimeout(() => {
+          setShowSuccessMessage(false)
+        }, 3000)
+      })
 
   }
 
   return (
     <div className='bg-white shadow-lg p-8 pb-12 mb-8 rounded-lg'>
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
-        CommentsForm
+        Share your opinion on that post
       </h3>
       <div className='grid grid-cols-1 gap-4 mb-4'>
         <textarea 
--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// import type { NextApiRequest, NextApiResponse } from 'next'
 
 
 /** *************************************************************
@@ -7,13 +7,15 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 * will be treated as an API endpoint instead of a page.         *
 *************************************************************** */
 
-import { GraphQLClient, gql } from 'graphql'
+import { GraphQLClient, gql } from 'graphql-request'
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+const graphcmsToken = process.env.GRAPHCMS_TOKEN
 
-export default function comments(req, res) {
-  const { name, email, comment, slug } = req.body
-  const graphQLClient = new GraphQLClient(graphqlAPI, {
+export default async function comments(req, res) {
+  // const { name, email, comment, slug } = req.body
+  console.log(graphcmsToken)
+  const graphQLClient = new GraphQLClient((graphqlAPI), {
     headers: {
       authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`
     }
@@ -24,8 +26,19 @@ export default function comments(req, res) {
       createComment(data: { name: $name, email: $email, comment: $comment, post: { connect: { slug: $slug }} }) { id }
     }
   `
-  
-  const result = await graphQLClient.request(query, req.body)
+  try {
+    // const result = await graphQLClient.request(query, req.body)
+    const result = await graphQLClient.request(query, {
+      name: req.body.name,
+      email: req.body.email,
+      comment: req.body.comment,
+      slug: req.body.slug,
+    })
 
-  return res.status(200).send(result)
+    return res.status(200).send(result)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send(error)
+  }
+  
 }
