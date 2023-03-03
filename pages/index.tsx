27cc67d6e66b02f2d906aac5bbef11e91d7fb989
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-03-03 08:23:27 +0100

    deploy failure - export default funtion Home 8

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ const posts = [
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post, index) => (
-            <PostCard post={post.node} key={index} />
+            <PostCard post={post} key={index} />
           ))}
         </div>
         
