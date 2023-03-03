13908ce9446867d8e2ac00e5db923f29726bdb7e
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-03-03 09:06:13 +0100

    deploy failure - export default funtion Home 10

--- pages/index.tsx
+++ pages/index.tsx
@@ -77,7 +77,8 @@ import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
 
-export default function Home({ posts }) {
+export default function Home({ posts }: any) {
+  
   return (
     <div className="container mx-auto px-10 mb-8">
       <FeaturedPosts />
