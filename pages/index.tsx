090831781edc3dc2d30769411e39618ab53c47a5
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-03-03 08:54:13 +0100

    deploy failure - export default funtion Home 9

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,73 +1,107 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { PostCard, Categories, PostWidget } from '../components'
+// import type { NextPage } from 'next'
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import { PostCard, Categories, PostWidget } from '../components'
 
-import { getPosts, getCategories } from '../services'
+// import { getPosts, getCategories } from '../services'
 
-import { FeaturedPosts } from '../sections/index'
+// import { FeaturedPosts } from '../sections/index'
 
-type PostsProps = {
-  title: string;
-  slug: string;
-}
-
-// const Posts: React.FunctionComponent<PostsProps> = (props) => {
-//   const {} = props
+// type PostsProps = {
+//   title: string;
+//   slug: string;
 // }
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing' },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
-];
+// // const Posts: React.FunctionComponent<PostsProps> = (props) => {
+// //   const {} = props
+// // }
 
-// export default function Home({ props: PostsProps }) {
-  const Home = (props: PostsProps) => {
-    // const { title, slug } = posts;
+// const posts = [
+//   { title: 'React Testing', excerpt: 'Learn React Testing' },
+//   { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
+// ];
+
+// // export default function Home({ props: PostsProps }) {
+//   const Home = (props: PostsProps) => {
+//     // const { title, slug } = posts;
   
   
-  return (
-    <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>CMS Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+//   return (
+//     <div className="container mx-auto px-10 mb-8">
+//       <Head>
+//         <title>CMS Blog</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
 
-      <FeaturedPosts />
+//       <FeaturedPosts />
 
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => (
-            <PostCard post={post} key={index} />
-          ))}
-        </div>
+//       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+//         <div className='lg:col-span-8 col-span-1'>
+//           {posts.map((post, index) => (
+//             <PostCard post={post} key={index} />
+//           ))}
+//         </div>
         
 
-        <div className='lg:col-span-4 col-span-1'>
-            <div className='lg:sticky relative top-8'>
-              <PostWidget />
-              <Categories />
-              {/* <Posts {...} /> */}
-            </div>
+//         <div className='lg:col-span-4 col-span-1'>
+//             <div className='lg:sticky relative top-8'>
+//               <PostWidget />
+//               <Categories />
+//               {/* <Posts {...} /> */}
+//             </div>
+//         </div>
+//       </div>
+
+      
+
+      
+//     </div>
+//   )
+// }
+
+
+// export default Home
+
+// export async function getStaticProps() {
+//   const posts = (await getPosts()) || [];
+
+//   return {
+//     props: { posts },
+//   }
+// }
+
+
+
+
+import { FeaturedPosts } from '../sections/index';
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
+
+export default function Home({ posts }) {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
-
-      
-
-      
     </div>
-  )
+  );
 }
 
-
-export default Home
-
+// Fetch data at build time
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
-
   return {
     props: { posts },
-  }
+  };
 }
-
-
